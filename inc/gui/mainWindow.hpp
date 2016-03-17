4ed23d6991b2e794d6e9323fb8100fc37913a88b
Ivan Bilych <ivanbilych@gmail.com>
2016-03-17 12:22:24 +0200

    gui: mainWindow: Add edit button press behaviour
    Also setup some dumb text in two list views
    
    Signed-off-by: Ivan Bilych <ivanbilych@gmail.com>

--- inc/gui/mainWindow.hpp
+++ inc/gui/mainWindow.hpp
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QStringListModel>
 
 #include <newIngridientWindow.hpp>
 #include <newDishWindow.hpp>
@@ -17,17 +18,32 @@ protected:
     void closeEvent(QCloseEvent *event) Q_DECL_OVERRIDE;
 
 public:
+    enum InfoType {
+        ITEM,
+        FOOD,
+        DISH,
+        NONE
+    };
+
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
 private slots:
     void on_pushButton_1_clicked();
     void on_pushButton_3_clicked();
+    void on_pushButton_5_clicked();
+    void on_listView_1_clicked(const QModelIndex &index);
+    void on_listView_2_clicked(const QModelIndex &index);
 
 private:
     Ui::MainWindow *ui;
     NewIngridientWindow *newIngridientWindow {};
     NewDishWindow *newDishWindow {};
+    InfoType infoWindowType {NONE};
+    QStringList *itemStringList {};
+    QStringList *dishStringList {};
+    QStringListModel *itemListModel {};
+    QStringListModel *dishListModel {};
 };
 
 #endif // MAINWINDOW_H
--- src/gui/mainWindow.cpp
+++ src/gui/mainWindow.cpp
@@ -5,12 +5,31 @@
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
-    ui(new Ui::MainWindow) {
+    ui(new Ui::MainWindow),
+    itemStringList(new QStringList),
+    dishStringList(new QStringList),
+    itemListModel(new QStringListModel(*itemStringList, NULL)),
+    dishListModel(new QStringListModel(*dishStringList, NULL)) {
 
     ui->setupUi(this);
 
     newIngridientWindow = new NewIngridientWindow();
     newDishWindow = new NewDishWindow();
+
+    ui->listView_1->setModel(itemListModel);
+    ui->listView_2->setModel(dishListModel);
+
+    itemStringList->append("Apple");
+    itemStringList->append("Potato");
+    itemStringList->append("Rice");
+    itemStringList->append("Nails");
+
+    dishStringList->append("Apple pie");
+    dishStringList->append("Fired Potato");
+    dishStringList->append("Rice with spices");
+
+    itemListModel->setStringList(*itemStringList);
+    dishListModel->setStringList(*dishStringList);
 
     PRINT_OBJ("MainWindow created");
 }
@@ -19,6 +38,10 @@ MainWindow::~MainWindow() {
     delete ui;
     delete newIngridientWindow;
     delete newDishWindow;
+    delete itemStringList;
+    delete dishStringList;
+    delete itemListModel;
+    delete dishListModel;
 
     PRINT_OBJ("MainWindow destroyed");
 }
@@ -34,4 +57,22 @@ void MainWindow::on_pushButton_1_clicked() {
 
 void MainWindow::on_pushButton_3_clicked() {
     newDishWindow->show();
+}
+
+void MainWindow::on_pushButton_5_clicked() {
+    if ( infoWindowType == ITEM ) {
+        newIngridientWindow->show();
+    } else if ( infoWindowType == FOOD ) {
+        newIngridientWindow->show();
+    } else if ( infoWindowType == DISH ) {
+        newDishWindow->show();
+    }
+}
+
+void MainWindow::on_listView_1_clicked(const QModelIndex &index) {
+    infoWindowType = ITEM;
+}
+
+void MainWindow::on_listView_2_clicked(const QModelIndex &index) {
+    infoWindowType = DISH;
 }
