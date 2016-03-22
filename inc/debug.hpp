4378cd955f42c2b00c2535e8cf23da94bd8f29eb
Ivan Bilych <ivanbilych@gmail.com>
2016-03-22 13:08:08 +0200

    debug: Print file/class debug info
    Compiling with PRINT_DEBUG_POSITION flag will force print debug file, line,
    class and function information
    
    Signed-off-by: Ivan Bilych <ivanbilych@gmail.com>

--- inc/debug.hpp
+++ inc/debug.hpp
@@ -2,6 +2,7 @@
 #define DEBUG_H
 
 #include <iostream>
+#include <typeinfo>
 
 #ifdef DEBUG
 #define NO_LOGS     0
@@ -19,38 +20,44 @@
 #define DEBUG_LEVEL NO_LOGS
 #endif // DEBUG
 
+#ifdef PRINT_DEBUG_POSITION
+#define POSITION __FILE__ << ":" << __LINE__ << " " << typeid(*this).name() << "::" << __FUNCTION__ << ": "
+#else
+#define POSITION ""
+#endif // PRINT_DEBUG_POSITION
+
 #if DEBUG_LEVEL > 0
-#define PRINT_ERR(x)   do { std::cerr << "[ERROR] " << x << std::endl; } while (false)
+#define PRINT_ERR(x)   do { std::cerr << "[ERROR] " << POSITION << x << std::endl; } while (false)
 #else
 #define PRINT_ERR(x)
 #endif
 
 #if DEBUG_LEVEL > 1
-#define PRINT_WARN(x)  do { std::cout << "[WARN]  " << x << std::endl; } while (false)
+#define PRINT_WARN(x)  do { std::cout << "[WARN]  " << POSITION << x << std::endl; } while (false)
 #else
 #define PRINT_WARN(x)
 #endif
 
 #if DEBUG_LEVEL > 2
-#define PRINT_INFO(x)  do { std::cout << "[INFO]  " << x << std::endl; } while (false)
+#define PRINT_INFO(x)  do { std::cout << "[INFO]  " << POSITION << x << std::endl; } while (false)
 #else
 #define PRINT_INFO(x)
 #endif
 
 #if DEBUG_LEVEL > 3
-#define PRINT_DEBUG(x) do { std::cout << "[DEBUG] " << x << std::endl; } while (false)
+#define PRINT_DEBUG(x) do { std::cout << "[DEBUG] " << POSITION << x << std::endl; } while (false)
 #else
 #define PRINT_DEBUG(x)
 #endif
 
 #if DEBUG_LEVEL > 4
-#define PRINT_OBJ(x)   do { std::cout << "[OBJ]   " << x << std::endl; } while (false)
+#define PRINT_OBJ(x)   do { std::cout << "[OBJ]   " << POSITION << x << std::endl; } while (false)
 #else
 #define PRINT_OBJ(x)
 #endif
 
-#define PRINT(x)       do { std::cout << "[MSG]   " << x << std::endl; } while (false)
-#define PRINT_TEST(x)  do { std::cout << "[TEST]  " << x << std::endl; } while (false)
+#define PRINT(x)       do { std::cout << "[MSG]   " << POSITION << x << std::endl; } while (false)
+#define PRINT_TEST(x)  do { std::cout << "[TEST]  " << POSITION << x << std::endl; } while (false)
 
 #define NAME_ID "\"" << getName() << "\" (" << getId() << ")"
 #define NAME_ID_CLASS(x) "\"" << (x).getName() << "\" (" << (x).getId() << ")"
