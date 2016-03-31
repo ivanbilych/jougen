bd50e6ff5a0fcc34b210720922259b03648eea84
Ivan Bilych <ivanbilych@gmail.com>
2016-03-31 17:24:07 +0300

    versionInfo: Add minor sub version
    This is required if there will be some fixes of additional work on release
    branch/ In this case we will increase minor_lo number
    
    Signed-off-by: Ivan Bilych <ivanbilych@gmail.com>

--- inc/versionInfo.hpp
+++ inc/versionInfo.hpp
@@ -4,7 +4,8 @@
 #include <string>
 
 #define VERSION_MAJOR "0"
-#define VERSION_MINOR "0"
+#define VERSION_MINOR_HI "0"
+#define VERSION_MINOR_LO "0"
 #define VERSION_SUFFIX " alpha"
 
 #ifndef COMMIT_ID
@@ -22,9 +23,10 @@
 #define PROJECT_URL "https://github.com/ivanbilych/jougen"
 
 const std::string versionMajor { VERSION_MAJOR };
-const std::string versionMinor { VERSION_MINOR };
+const std::string versionMinorHi { VERSION_MINOR_HI };
+const std::string versionMinorLo { VERSION_MINOR_LO };
 const std::string versionSuffix { VERSION_SUFFIX };
-const std::string programVersion { versionMajor + "." + versionMinor + versionSuffix };
+const std::string programVersion { versionMajor + "." + versionMinorHi + "." + versionMinorLo + versionSuffix };
 const std::string buildDate { BUILD_DATE };
 const std::string buildTime { BUILD_TIME };
 const std::string buildTimestamp { buildDate + " " + buildTime };
